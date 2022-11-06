4a08bff43621b933e99b26480a23fdf914012967
kachmura <chmuraka1998@gmail.com>
2022-11-06 15:32:22 +0100

    nowy feature - wprowadzanie imienia z konsoli

--- src/MyName.java
+++ src/MyName.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class MyName {
     public static void main(String[] args) {
-        String name = "Marcin";
+        String name;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Podaj swoje imię: ");
+        name = s.next();
         System.out.println("Moje imię to: " + name);
     }
 }
